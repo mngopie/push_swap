5c8d92a978dadad1662a9a4d9a86f7779af8b411
Pixel <pixel@Pixels-MacBook-Air.local>
2026-02-26 15:15:33 +0100

    Thurs

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pjakosal <pjakosal@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pixel <pixel@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 13:11:52 by pixel             #+#    #+#             */
-/*   Updated: 2026/02/25 19:37:52 by pjakosal         ###   ########.fr       */
+/*   Updated: 2026/02/26 14:54:24 by pixel            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,14 +41,16 @@ void	print_stack(t_node *stack)
 	}
 }
 
-int	stack_sorted(t_node **stack_a)
+int	is_sorted(t_node **stack_a)
 {
 	t_node *current;
 	
 	current = *stack_a;
-	while (current->next != NULL)
+	if (!stack_a || !*stack_a)
+		return (0);
+	while (current->next)
 	{
-		if (current->data < current->next->data)
+		if (current->data > current->next->data)
 			// now compare if the current->next->data < current->next->next->data
 			// amo na ang problem if indi ko kabalo how many arguments are there
 			// need ko ang size and to know when to go in this function
@@ -75,7 +77,7 @@ int	main(int argc, char **argv)
 	if (!args)
 		return (write(2, "Error\n", 6), 1);
 	handle_args(&stack_a, args, count);
-	if (!stack_sorted(&stack_a)) // is this just a check? or diri ko himuon ang manual sorting for 3, 4, 5 arguments?
+	if (!is_sorted(&stack_a)) // is this just a check? or diri ko himuon ang manual sorting for 3, 4, 5 arguments?
 		// it would go through sorting logic - algorithm
 	// if (argc == 2) // free split_or_not(args)
 	// 	free (split_or_not);
